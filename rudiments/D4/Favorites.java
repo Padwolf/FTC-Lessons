c4be1828086747333e3d85221d2baee19d35cb5f
Padwolf <padwolf99@gmail.com>
2017-04-13 15:26:34 -0600

    Added Favorites program

---
+++ rudiments/D4/Favorites.java
@@ -0,0 +1,22 @@
+// Patrick Duane
+// The Rudiments of Java
+// Day 4: Favorites
+
+import java.io.*;
+
+public class Favorites {
+	public static void main(String[] args) {
+		System.out.println("Patrick Duane");
+		System.out.println("The Rudiments of Java");
+		System.out.println("Day 4: Favorites");
+		System.out.println();
+		
+		String favoriteShow = "MacGyver";
+		char favoriteLetter = 'P';
+		int favoriteYear = 1999;
+		
+		System.out.println("Favorite Show: " + favoriteShow);
+		System.out.println("Favorite Letter: " + favoriteLetter);
+		System.out.println("Favorite Year: " + favoriteYear);
+	}
+}
